3bb751ea2d887fd3230251cb3f45e31270d876b1
Dmitriy4ub <example@gmail.com>
2024-05-28 14:54:55 +0200

    28.05.2024rabotasmenu

--- goods/views.py
+++ goods/views.py
@@ -1,8 +1,86 @@
 from django.shortcuts import render
+from django.template import context
 
 
 def catalog(request):
-    return render(request, "goods/catalog.html")
+    context: dict[str, str] = {
+        "title": "Каталог товаров",
+        "goods": [
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-5h7-9-11-13-sm-4-sht-27378-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-5h7-9-11-13-sm-4-sht-27382-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+            {
+                "image": "deps\img\svechi\komplekt-krasnyh-svechey-bispol-tsilindr-4h6-sm-4-sht-27366-1-660x660.jpg",
+                "name": "Комплект цилиндрических свечей Bispol",
+                "description": "Комплект цилиндрических свечей Bispol",
+                "price": 400,
+            },
+        ],
+    }
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request):
